4bceb60c775faf79a539c76074849d8fcdc42e73
하정원 <sch20215104@gmail.com>
2022-09-09 18:24:25 +0900

    2-1

---
+++ Cpp week 02/2-1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	char Alphabet;
+	int vowel = 0, consonants = 0;
+
+	cout << "영문자를 입력하고 Ctrl+Z를 치세요" << endl;
+
+	while (cin >> Alphabet) {
+		switch (Alphabet) {
+		case 'a':
+		case 'e':
+		case 'i':
+		case 'o':
+		case 'u':
+			vowel++;
+			break;
+		default:
+			consonants++;
+			break;
+		}
+	}
+	cout << "모음 : " << vowel << endl;
+	cout << "자음 : " << consonants << endl;
+}
